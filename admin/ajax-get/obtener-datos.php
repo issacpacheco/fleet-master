80255869e0f889e1c37d0d8f4cf0da6896389308
IssacPacheco <isaacpacheco.go@gmail.com>
2022-09-26 19:41:00 -0500

    Se inician consultas para generar reportes

--- admin/ajax-get/obtener-datos.php
+++ admin/ajax-get/obtener-datos.php
@@ -3,7 +3,7 @@ session_start();
 if(isset($_SESSION['hash'])){
 		$trackerID = filter_input(INPUT_POST, 'tracker_id', FILTER_SANITIZE_NUMBER_INT);
 ?>
-<style>
+<!-- <style>
 	.highcharts-figure .chart-container {
     width: 300px;
     height: 200px;
@@ -64,156 +64,240 @@ if(isset($_SESSION['hash'])){
         margin: 0 auto;
     }
 }
-</style>
-<figure class="highcharts-figure">
-    <!-- <div id="container-speed" class="chart-container"></div> -->
-    <!-- <div id="container-rpm" class="chart-container"></div> -->
-	<input type="hidden" name="" id="hash" value="<?php echo $_SESSION['hash']; ?>">
-	<input type="hidden" name="" id="trackerid" value="<?php echo $trackerID; ?>">
-	<div id="container"></div>
-    <p class="highcharts-description">
-        Grafica de gasolina del dispositivo <?php echo $trackerID ?>
-    </p>
-</figure>
+</style> -->
+<div class="row">
+	<div class="col-sm-4">
+		<label>Titulo del reporte</label>
+		<input type="text" name="nombre_reporte" id="nombre_reporte" value="" placeholder="Nombre que tendra el reporte" class="form-control">
+	</div>
+	<div class="col-sm-4">
+		<label>Rango de fechas</label>
+		<div class="input-group">
+			<div class="input-group-addon">
+				<i class="fa fa-calendar"></i>
+			</div>
+			<input type="text" class="form-control pull-right" id="fechas" name="fechas" readonly>
+		</div>
+	</div>
+	<div class="col-sm-3">
+		<label></label>
+		<button class="btn btn-success form-control" onclick="generarreporte();">Generar reporte</button>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-12">
+		<figure class="highcharts-figure">
+			<div id="container-speed" class="chart-container"></div>
+			<!-- <div id="container-rpm" class="chart-container"></div> -->
+			<div id="container"></div>
+			<p class="highcharts-description">
+				Grafica de gasolina del dispositivo <?php echo $trackerID ?>
+			</p>
+		</figure>
+	</div>
+</div>
+
+<script src="plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<script>
+	$(document).ready(function(){
+		$('#fechas').on('cancel.daterangepicker', function(ev, picker) {//do something, like clearing an input
+			$('#fechas').val('');
+		});
+
+		
+		$('#fechas').daterangepicker({
+			"locale": {
+				"format": "YYYY-MM-DD HH:mm:ss:ss",
+				"separator": " / ",
+				"applyLabel": "Seleccionar",
+				"cancelLabel": "Cancelar",
+				"fromLabel": "De",
+				"toLabel": "Al",
+				"customRangeLabel": "Custom",
+				"daysOfWeek": [
+					"Do",
+					"Lu",
+					"Ma",
+					"Mi",
+					"Ju",
+					"Vi",
+					"Sa"
+				],
+				"monthNames": [
+					"Enero",
+					"Febrero",
+					"Marzo",
+					"Abril",
+					"Mayo",
+					"Junio",
+					"Julio",
+					"Agosto",
+					"Septiembre",
+					"Octubre",
+					"Noviembre",
+					"Diciembre"
+				],
+				"firstDay": 1
+			}
+		});
+	});
+	
+</script>
 <script>
 	// window.onload = cargargrafica();
-	$(document).ready(function(){
-		cargargrafica();
-	})
-	function cargargrafica(){
-		var hash = document.getElementById("hash").value;
-		var trackerid = document.getElementById("trackerid").value;
-		Highcharts.chart('container', {
-			chart: {
-				type: 'gauge',
-				plotBackgroundColor: null,
-				plotBackgroundImage: null,
-				plotBorderWidth: 0,
-				plotShadow: false,
-				height: '80%'
-			},
+	var gaugeOptions = {
+		chart: {
+			type: 'solidgauge'
+		},
 
+		title: null,
+
+		pane: {
+			center: ['50%', '85%'],
+			size: '140%',
+			startAngle: -90,
+			endAngle: 90,
+			background: {
+				backgroundColor:
+					Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+				innerRadius: '60%',
+				outerRadius: '100%',
+				shape: 'arc'
+			}
+		},
+
+		exporting: {
+			enabled: false
+		},
+
+		tooltip: {
+			enabled: false
+		},
+
+		// the value axis
+		yAxis: {
+			stops: [
+				[0.1, '#55BF3B'], // green
+				[0.5, '#DDDF0D'], // yellow
+				[0.9, '#DF5353'] // red
+			],
+			lineWidth: 0,
+			tickWidth: 0,
+			minorTickInterval: null,
+			tickAmount: 2,
 			title: {
-				text: 'Gasolina'
+				y: -70
 			},
+			labels: {
+				y: 16
+			}
+		},
 
-			pane: {
-				startAngle: -90,
-				endAngle: 89.9,
-				background: null,
-				center: ['50%', '75%'],
-				size: '110%'
-			},
-
-			// the value axis
-			yAxis: {
-				min: 0.00,
-				max: 150.00,
-				tickPixelInterval: 72,
-				tickPosition: 'inside',
-				tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
-				tickLength: 20,
-				tickWidth: 2,
-				minorTickInterval: null,
-				labels: {
-					distance: 20,
-					style: {
-						fontSize: '14px'
-					}
-				},
-				// stops: [
-				// 	[0.1, '#DF5353'], // red DF5353
-				// 	[0.5, '#DDDF0D'], // yellow
-				// 	[0.9, '#55BF3B'] // green 55BF3B
-				// ],
-				plotBands: [{
-					from: 0,
-					to: 120,
-					color: '#DF5353', // red
-					thickness: 20
-				}, {
-					from: 120,
-					to: 160,
-					color: '#DDDF0D', // yellow
-					thickness: 20
-				}, {
-					from: 160,
-					to: 200,
-					color: '#55BF3B', // green
-					thickness: 20
-				}]
-			},
-
-			series: [{
-				name: 'Speed',
-				data: [0],
-				tooltip: {
-					valueSuffix: ' Litros'
-				},
+		plotOptions: {
+			solidgauge: {
 				dataLabels: {
-					format: '{y} Litros',
+					y: 5,
 					borderWidth: 0,
-					color: (
-						Highcharts.defaultOptions.title &&
-						Highcharts.defaultOptions.title.style &&
-						Highcharts.defaultOptions.title.style.color
-					) || '#333333',
-					style: {
-						fontSize: '16px'
-					}
-				},
-				dial: {
-					radius: '80%',
-					backgroundColor: 'gray',
-					baseWidth: 12,
-					baseLength: '0%',
-					rearLength: '0%'
-				},
-				pivot: {
-					backgroundColor: 'gray',
-					radius: 6
+					useHTML: true
 				}
-			}]
-		});
-		setInterval(() => {
-			$.ajax({
-				type: "POST",
-				url: "https://api.navixy.com/v2/tracker/readings/list",
-				// dataType: "json",
-				data: {hash: '<?php echo $_SESSION['hash'] ?>', tracker_id: <?php echo $trackerID; ?>},
-				success: function (response) {
-					console.log(response);
-					for(var a = 0; a < response.inputs.length; a++){
-						if(response.inputs[a].label == 'DIESEL'){
-							const chart = Highcharts.charts[0];
-							valor_min = Number(response.inputs[a].min_value);
-							valor_max = Number(response.inputs[a].max_value);
-							chart.yAxis[0].setExtremes(valor_min,valor_max);
-						}
-					}
-					// setInterval(() => {
-						const chart = Highcharts.charts[0];
-						if (chart && !chart.renderer.forExport) {
-							const point = chart.series[0].points[0];
-							for(var b = 0; b < response.inputs.length; b++){
-							if(response.inputs[b].label == 'DIESEL'){
-									inc = Number(response.inputs[b].value);
+			}
+		}
+	};
+	$.ajax({
+			type: "POST",
+			url: "https://api.navixy.com/v2/tracker/readings/list",
+			// dataType: "json",
+			data: {hash: '<?php echo $_SESSION['hash'] ?>', tracker_id: <?php echo $trackerID; ?>},
+			success: function (response) {
+				for(var a = 0; a < response.inputs.length; a++){
+					if(response.inputs[a].label == 'DIESEL'){
+						var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+							yAxis: {
+								min: response.inputs[a].min_value,
+								max: response.inputs[a].max_value,
+								title: {
+									text: 'Gasolina'
 								}
+							},
+
+							credits: {
+								enabled: false
+							},
+
+							series: [{
+								name: 'Gasolina',
+								data: [0],
+								dataLabels: {
+									format:
+										'<div style="text-align:center">' +
+										'<span style="font-size:25px">{y}</span><br/>' +
+										'<span style="font-size:12px;opacity:0.4">Listros</span>' +
+										'</div>'
+								},
+								tooltip: {
+									valueSuffix: ' Lts'
+								}
+							}]
+
+						}));
+						setInterval(function () {
+							var point,
+								newVal,
+								inc;
+							if (chartSpeed) {
+								point = chartSpeed.series[0].points[0];
+								for(var b = 0; b < response.inputs.length; b++){
+									if(response.inputs[b].label == 'DIESEL'){
+										inc = Number(response.inputs[b].value);
+									}
+								}
+								point.update(inc);
 							}
-							point.update(inc);
-						}
-					// }, 1000);
-				},
-				failure: function (response) {
-
-				},
-				error: function (response) {
-
+						}, 1000);
+					}
 				}
-			});
-		}, 5000);
-	}
+			},
+			failure: function (response) {
+
+			},
+			error: function (response) {
+
+			}
+		})
+
 	
+</script>
+<script>
+	function generarreporte(){
+		var titulo 	= document.getElementById("nombre_reporte").value;
+		var fecha	= document.getElementById("fechas").value;
+		const rango_fecha = fecha.split(' / ');
+		var hash 	= "<?php echo $_SESSION['hash'] ?>";
+		var tracker = <?php echo $trackerID ?>;
+		var from	= rango_fecha[0];
+		var to		= rango_fecha[1];
+		var timefil = {"from": "00:00:00", "to": "23:59:59", "weekdays": [1,2,3,4,5,6,7]};
+		var plugin  = {"details_interval_seconds": 300, "plugin_id": 9, "show_seconds": false, "graph_type": "time", "smoothing": false, "sensors": [{"tracker_id": tracker, "sensor_id": 1869214}]};
+
+		var datos 	= "hash="+encodeURI(hash)+
+					  "&title="+encodeURI(titulo)+
+					  "&trackers=["+tracker+"]&from="+encodeURI(from)+
+					  "&to="+encodeURI(to)+
+					  "&time_filter="+JSON.stringify(timefil)+
+					  "&plugin="+JSON.stringify(plugin);
+
+		// console.log(datos);
+		
+		$.ajax({
+			type: 'POST',
+			url: 'https://api.navixy.com/v2/report/tracker/generate?'+datos,
+			success: function(response){
+				console.log(response);
+			}
+		})
+	}
 </script>
 <?php
 }else{
--- admin/index.php
+++ admin/index.php
@@ -19,7 +19,7 @@ include("ajax-get/obtener-lista.php");
     <link rel="stylesheet" href="addons/bootstrap/css/bootstrap.css"/>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.0/css/all.css">
     <link rel="stylesheet" href="styles/style.css"/>
-	<link rel="stylesheet" href="styles/theme-dark.css" class="theme" />
+	<link rel="stylesheet" href="styles/theme-blue.css" class="theme" />
     <!-- End of Styling -->
 	<link rel="stylesheet" href="scripts/dropzone-5.7.0/dist/dropzone.css">
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/af-2.2.2/b-1.5.1/b-colvis-1.5.1/b-flash-1.5.1/b-html5-1.5.1/b-print-1.5.1/cr-1.4.1/fc-3.2.4/fh-3.1.3/kt-2.3.2/r-2.2.1/rg-1.0.2/rr-1.2.3/sc-1.4.4/sl-1.2.5/datatables.min.css"/>
@@ -43,6 +43,7 @@ include("ajax-get/obtener-lista.php");
 	<!----Charts---->
 	<script src="https://code.highcharts.com/highcharts.js"></script>
 	<script src="https://code.highcharts.com/highcharts-more.js"></script>
+	<script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
 	<script src="https://code.highcharts.com/modules/exporting.js"></script>
 	<script src="https://code.highcharts.com/modules/export-data.js"></script>
 	<script src="https://code.highcharts.com/modules/accessibility.js"></script>
